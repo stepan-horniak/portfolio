638797f1ec31a6f8b9d494877f0e95164982520e
stepan <stepanhornjak@gmail.com>
2025-10-14 23:36:53 +0300

    go-go

--- js/script.js
+++ js/script.js
@@ -6,14 +6,36 @@ function windowLoaded() {
   }
   addEventListener("click", actions)
 
+  //====================hero text print=========
   const heroText = document.querySelector(".info-hero__text")
-  const printText = heroText.textContent
-  heroText.textContent = ""
+  if (heroText) {
+    const printText = heroText.textContent
+    const heightText = heroText.offsetHeight
+    heroText.style.height = `${heightText}px`
+    heroText.textContent = ""
 
-  for (let i = 0; i < printText.length; i++) {
-    setTimeout(() => {
-      heroText.textContent += printText[i]
-    }, i * 30)
+    for (let i = 0; i < printText.length; i++) {
+      setTimeout(() => {
+        heroText.textContent += printText[i]
+      }, i * 25)
+    }
   }
+
+  //=================header background color =========
+
+  // window.addEventListener("scroll", (e) => {
+  //   const headerWrapper = document.querySelector(".header__wrapper")
+  //   headerWrapper.style.backgroundColor = "#3f3d3d"
+  //   console.log(e.target)
+  // })
+  function handleScroll(e) {
+    const headerWrapper = document.querySelector(".header__wrapper")
+    headerWrapper.style.backgroundColor = "#3f3d3d"
+
+    window.removeEventListener("scroll", handleScroll)
+  }
+
+  window.addEventListener("scroll", handleScroll)
+  //===========================================================
 }
 addEventListener("load", windowLoaded)
