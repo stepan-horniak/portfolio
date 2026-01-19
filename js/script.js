0ed6d1a887c0aeb29c264da36a88c30c72914f4a
stepan <stepanhornjak@gmail.com>
2026-01-19 20:58:56 +0200

    test message-bot

--- js/script.js
+++ js/script.js
@@ -36,6 +36,32 @@ function windowLoaded() {
   }
 
   window.addEventListener("scroll", handleScroll)
+
+  //====================
+
+  const TELEGRAM_TOKEN = "8473647684:AAHBh_JE0fYrfH_-8K0rn9VQErluwnwkroY"
+  const CHAT_ID = "YOUR_CHAT_ID"
+
+  function sendTelegramMessage(message) {
+    const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        text: message,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log("Telegram response:", data))
+      .catch((err) => console.error("Error sending message:", err))
+  }
+
+  // Викликаємо при завантаженні сторінки
+  window.addEventListener("load", () => {
+    const message = `Хтось зайшов на сайт!`
+    sendTelegramMessage(message)
+  })
   //===========================================================
 }
 addEventListener("load", windowLoaded)
