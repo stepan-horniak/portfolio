135c830bcae576f7505fa7735d358eb818edff80
stepan <stepanhornjak@gmail.com>
2026-04-09 20:16:30 +0300

    test

--- sw.js
+++ sw.js
@@ -1,7 +1,12 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("app-cache").then((cache) => {
-      return cache.addAll(["/", "/index.html", "/styles.css", "/script.js"])
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/css/style.css",
+        "/js/script.js",
+      ])
     }),
   )
 })
